2edf99c94433a474639dc81a5e8638bb7047378c
chalanaK95 <ChalanaK@fortude.co>
2022-06-30 00:02:08 +0530

    form created

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import Post from './Post/Post'
+import useStyles from './style'
+import { useSelector } from 'react-redux'
 
 const Posts = () => {
+
+  const posts = useSelector((state) => state.posts)
+  const classes = useStyles()
+
+  console.log(posts);
   return (
     <>
       <div>Posts</div>
