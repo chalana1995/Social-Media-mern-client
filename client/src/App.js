ab14d911bf1cd3d4711ffcc6e8439c9cb5c616d8
chalanaK95 <ChalanaK@fortude.co>
2022-06-29 23:28:44 +0530

    set redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+import {getPosts} from './actions/posts';
 import memories from './images/memories.png'
 import Form from './components/Form/Form.js';
 import Posts from './components/Posts/Posts';
@@ -8,6 +10,13 @@ import useStyles from './styles'
 
 const App = () => {
     const classes = useStyles()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+       dispatch(getPosts());
+    }, [dispatch]);
+
+
     return (
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position='static' color='inherit'>
@@ -18,10 +27,10 @@ const App = () => {
                 <Container>
                     <Grid container justify='space-between' alignItems='stretch' spacing={3}>
                         <Grid item xs={12} sm={7}>
-                            <Posts/>
+                            <Posts />
                         </Grid>
                         <Grid item xs={12} sm={4}>
-                            <Form/>
+                            <Form />
                         </Grid>
                     </Grid>
                 </Container>
