dd4ef0a2deca25938f96221936ad03ef8424f9e7
chalanaK95 <ChalanaK@fortude.co>
2022-07-09 20:06:07 +0530

    add css

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -8,20 +8,38 @@ import useStyles from './style'
 import moment from 'moment';
 
 const Post = ({ post }) => {
+
+  console.log("==== post 435===",post);
   const classes = useStyles()
   return (
     <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
       <div className={classes.overlay}>
         <Typography variant='h6'>{post.creator}</Typography>
-        <Typography variant='body2'>{moment(post.createdAt).fromNow}</Typography>
+        <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
       </div>
       <div className={classes.overlay2}>
-        <Button style={{ color: 'white' }} size="small" onClick={() => { }}></Button>
+        <Button style={{ color: 'white' }} size="small" onClick={() => { }}>
+          <MoreHorizIcon fontSize='default' />
+        </Button>
       </div>
       <div className={classes.details}>
-          <Typography variant='body2' color="textSecondary">{post.tags.map((tag) => `#${tag}`)}</Typography>
+        <Typography variant='body2' color="textSecondary">{post.tags.map((tag) => `#${tag}`)}</Typography>
       </div>
+      <CardContent>
+        <Typography className={classes.title} variant='h5' color="textSecondary">{post.message}</Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={() => { }}>
+          <ThumbUpAltIcon fontSize='small' />
+          Like
+          {post.likeCount}
+        </Button>
+        <Button size="small" color="primary" onClick={() => { }}>
+          <DeleteIcon fontSize='small' />
+          Delete
+        </Button>
+      </CardActions>
     </Card>
   )
 }
