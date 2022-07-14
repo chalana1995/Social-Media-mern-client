2dfb72d43d0a8a4d47d772de5e7a5df297ac138b
chalanaK95 <ChalanaK@fortude.co>
2022-07-14 21:38:43 +0530

    delete post done

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -6,10 +6,14 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import useStyles from './style'
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
+import { deletePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
 
-  console.log("==== post 435===",post);
+
+  const dispatch = useDispatch();
+
   const classes = useStyles()
   return (
     <Card className={classes.card}>
@@ -38,7 +42,7 @@ const Post = ({ post, setCurrentId }) => {
           Like
           {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => { }}>
+        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize='small' />
           Delete
         </Button>
