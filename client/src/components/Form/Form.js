5a32d6149782d2ebec0ef95b6d97b43493feaf62
chalanaK95 <ChalanaK@fortude.co>
2022-07-24 23:52:10 +0530

    bug fixing

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -11,12 +11,12 @@ const Form = ({ currentId, setCurrentId }) => {
   const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
 
   const [postData, setPostData] = useState({
-    creator: '',
     title: '',
     message: '',
     tags: '',
     selectedFile: ''
   })
+  const user = JSON.parse(localStorage.getItem('profile'))
 
   useEffect(() => {
     if (post) {
@@ -28,32 +28,35 @@ const Form = ({ currentId, setCurrentId }) => {
     e.preventDefault();
 
     if (currentId) {
-      dispatch(updatePost(currentId, postData))
+      dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }));
     }
     else {
-      dispatch(createPost(postData))
+      dispatch(createPost({ ...postData, name: user?.result?.name }));
     }
     clear()
   }
 
   const clear = () => {
     setCurrentId(null);
-    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' })
+    setPostData({title: '', message: '', tags: '', selectedFile: '' })
   }
+
+  if(!user?.result.name)
+  {
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant='h6' align='center'>
+         Please Sign In to create your own memories and like other's memories
+        </Typography>
+      </Paper>
+    )
+  }
+
 
   return (
     <Paper className={classes.paper}>
       <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
         <Typography variant='h6'>{currentId ? "Editing" : "Creating"} a Memory</Typography>
-        <TextField
-          name='creator'
-          className={classes.fileInput}
-          variant='outlined'
-          label='Creater'
-          fullWidth
-          value={postData.creator}
-          onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
-        />
         <TextField
           name='title'
           className={classes.fileInput}
